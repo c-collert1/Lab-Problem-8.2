6f47e9114df3ddc198002135bd2047046d845054
Caleb Collert <c-collert.1@onu.edu>
2024-03-28 10:22:29 -0400

    Completed countWords function

--- P8-2/P8-2.cpp
+++ P8-2/P8-2.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+int countWords(string str);
 
 int main() {
 
+	cout << countWords("one two three 4    5 6 7 eight");
 
+}
 
+int countWords(string str) {
+	int wordCounter = 0;
+	if (str.length() < 1) {
+		wordCounter = 0;
+	}
+	else {
+		wordCounter = 1;
+	}
+
+	for (int i = 0; i < str.length(); i++) {
+		string previousLetter = "";
+		string currentLetter = str.substr(i, 1);
+		if (i > 0) {
+			previousLetter = str.substr(i - 1, 1);
+		}
+		if (currentLetter == " " && (previousLetter != " ")) {
+			wordCounter++;
+		}
+	}
+	return wordCounter;
 }
